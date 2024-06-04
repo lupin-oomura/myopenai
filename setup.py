95fe8535f4e371f06e9cbba937171d5591e69c3d
Lupin-oomura <lupin.oomura@gmail.com>
2024-06-04 21:50:09 +0900

    add get_nextcommand

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myopenai',
-    version='0.4.10',
+    version='0.4.11',
     packages=find_packages(),
     install_requires=[
         'openai',
