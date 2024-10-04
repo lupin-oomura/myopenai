37e0be9253212ade546902ed37c51ad4b4070b47
Shin Oomura <lupin.oomura@example.com>
2024-10-04 10:52:07 +0900

    structuredoutput

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myopenai',
-    version='0.7.7',
+    version='0.8.0',
     packages=find_packages(),
     install_requires=[
         'openai',
